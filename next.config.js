2b8ad05674b0ba1e65dabad248d750000549d67f
soberdeer <antongraywing@gmail.com>
2023-08-21 19:05:30 +0300

    type fixes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 module.exports = () => ({
-    publicRuntimeConfig: {
-      NODE_ENV: process.env.NODE_ENV,
-    },
     images: {
       domains: ['https://l.evidon.com', 'https://c.evidon.com', 'https://d3awvtnmmsvyot.cloudfront.net/api'],
     },
